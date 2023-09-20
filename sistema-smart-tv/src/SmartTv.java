9ed3a6851c4dfdd1d9ace73d4a33d4d6f38e28ba
Jhoseph <jhosephazeredo@gmail.com>
2023-09-20 14:22:28 -0300

    Tv

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,34 @@
+public class SmartTv {
+    public final String Volume = null;
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+    }
+
+    public void desligar() {
+        ligada = false;
+    }
+
+        public void aumentarVolume() {
+        volume ++;
+    }
+    
+    public void diminuirVolume() {
+        volume --;
+    }
+
+    public void mudarCanal (int novoCanal) {
+        canal = novoCanal;
+    }
+    
+    public void aumentarCanal () {
+        
+    }
+
+    public void diminuirCanal () {
+        
+    }
+}
